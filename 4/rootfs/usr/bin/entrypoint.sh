848280a6c51bd669eb470831723e2151363bafc8
Christian Bönning <christian@verloren-im.net>
2021-12-23 13:19:50 +0100

    Update 4/Dockerfile, 4/rootfs/usr/bin/entrypoint.sh, .gitlab-ci.yml Deleted run-tests.sh, DEVELOPER.md, README.md, LICENSE, .travis.yml

--- 4/rootfs/usr/bin/entrypoint.sh
+++ 4/rootfs/usr/bin/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-declare -a args
+declare -a args=()
 
 add_env_var_as_env_prop() {
   if [ "$1" ]; then
@@ -10,16 +10,23 @@ add_env_var_as_env_prop() {
   fi
 }
 
-add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
-add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
-add_env_var_as_env_prop "${SONAR_USER_HOME:-}" "sonar.userHome"
-add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
-
-PROJECT_BASE_DIR="${PWD}"
-if [ "${SONAR_PROJECT_BASE_DIR:-}" ]; then
-  PROJECT_BASE_DIR="${SONAR_PROJECT_BASE_DIR}"
+# if nothing is passed, assume we want to run sonar-scanner
+if [[ "$#" == 0 ]]; then
+  set -- sonar-scanner
 fi
 
-export SONAR_USER_HOME="${PROJECT_BASE_DIR}/.sonar"
+# if first arg looks like a flag, assume we want to run sonar-scanner with flags
+if [[ "${1#-}" != "${1}" ]] || [[ -z "$(command -v "${1}")" ]]; then
+  set -- sonar-scanner "$@"
+fi
 
-sonar-scanner "${args[@]}"
+if [[ "$1" = 'sonar-scanner' ]]; then
+  add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
+  add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
+  add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
+  if [ ${#args[@]} -ne 0 ]; then
+    set -- sonar-scanner "${args[@]}" "${@:2}"
+  fi
+fi
+
+exec "$@"
